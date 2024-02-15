ebcc3d6fc4ea2e786747765476736a3cbbba3d56
Kazım Aydin <kzmaydn@hotmail.com>
2024-02-15 15:06:22 +0300

    MealCard component

---
+++ app/views/components/meal_card_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class MealCardComponent < ApplicationComponent
+  def template
+    h1 { "MealCard" }
+    p { "Find me in app/views/components/meal_card_component.rb" }
+  end
+end
