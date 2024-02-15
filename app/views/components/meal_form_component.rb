b9d48f80f5156bebadcd33ac10951b470e97f14a
Kazım Aydin <kzmaydn@hotmail.com>
2024-02-15 16:52:52 +0300

    new Meal component

---
+++ app/views/components/meal_form_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class MealFormComponent < ApplicationComponent
+  def template
+    h1 { "MealForm" }
+    p { "Find me in app/views/components/meal_form_component.rb" }
+  end
+end
